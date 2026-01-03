4f7048f0dea2ccb9377fe633e4cbabb89e0542bb
Erik Amorim <erik.benvindo@gmail.com>
2026-01-02 21:37:58 -0300

    Atualização do desemvolvimento do código.

--- automacao_web_selenium/src/main.py
+++ automacao_web_selenium/src/main.py
@@ -1,15 +1,27 @@
 import pandas as pd
-from selenium import webdriver
+import undetected_chromedriver as uc  # Essa biblioteca faz com quê o Google não reconheça o robô.
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from pathlib import Path
 
 ROOT_PATH = Path(__file__).parent.parent
 
-nav = webdriver.Chrome()
+options = uc.ChromeOptions()
+nav = uc.Chrome(options=options)
 
 tabela_produtos = pd.read_excel("assets/buscas.xlsx")
 
 nav.get("https://google.com")
 
 produto = "Iphone 12 64GB"
-nav.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div/div[2]/input').send_keys(produto, Keys.ENTER)
+nav.find_element("xpath", "//*[@id='APjFqb']").send_keys(produto, Keys.ENTER)
+elemento = nav.find_element(
+    By.PARTIAL_LINK_TEXT, "Shopping"
+)  # Como a classe muda sempre, consegui pegar utilizando parte do nome do link.
+elemento.click()
+
+
+try:
+    input("Pressione Enter no terminal para fechar o navegador...")
+finally:
+    nav.quit()
