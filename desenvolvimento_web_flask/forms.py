86486496583179a54d6846ea881856e9f8478a84
Erik Amorim <erik.benvindo@gmail.com>
2026-02-18 23:19:05 -0300

    Projeto de site utilizando Flask

---
+++ desenvolvimento_web_flask/forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, SubmitField
+from wtforms.validators import DataRequired, Email, Length, EqualTo
+
+
+class FormCriarConta(FlaskForm):
+    username = StringField("Nome de Usuário", validators=[DataRequired()])
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    senha = PasswordField("Senha", validators=[DataRequired(), Length(6, 20)])
+    confirmacao_senha = PasswordField(
+        "Confirmação de Senha", validators=[DataRequired(), EqualTo("senha")]
+    )
+    botao_submit_criarconta = SubmitField("Criar Conta")
+
+
+class FormLogin(FlaskForm):
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    senha = PasswordField("Senha", validators=[DataRequired(), Length(6, 20)])
+    botao_submit_login = SubmitField("Fazer Login")
---
+++ desenvolvimento_web_flask/main..py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, url_for
+from contextvars import Context
+
+app = Flask(__name__)
+
+lista_usuarios = ["Erik", "Lim", "Jose", "Maria", "Joao"]
+lista_usuarios.sort()
+
+app.config("SECRET_KEY") = "d718df7d5300f2ec0a950cd9b804445b"
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/contato")
+def contato():
+    return render_template("contato.html")
+
+
+@app.route("/usuarios")
+def usuarios():
+    return render_template("usuarios.html", lista_usuarios=lista_usuarios)
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
