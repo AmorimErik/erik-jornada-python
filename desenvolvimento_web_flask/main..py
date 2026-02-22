364a163f5b617725a24791df4f2b4b8b59e8644c
Erik Amorim <erik.benvindo@gmail.com>
2026-02-22 16:57:26 -0300

    Criação do banco de dados

--- desenvolvimento_web_flask/main..py
+++ desenvolvimento_web_flask/main..py
@@ -1,6 +1,21 @@
-from flask import Flask, render_template, url_for
+from flask import (
+    Flask,
+    flash,
+    redirect,
+    render_template,
+    request,
+    url_for,
+)
 from contextvars import Context
 from forms import FormCriarConta, FormLogin
+from flask_sqlalchemy import SQLAlchemy
+from pathlib import Path
+
+basedir = Path(__file__).resolve().parent
+data_dir = basedir / "data"
+data_dir.mkdir(exist_ok=True)
+
+db_path = data_dir / "projetosite.db"
 
 app = Flask(__name__)
 
@@ -8,6 +23,9 @@ lista_usuarios = ["Erik", "Lim", "Jose", "Maria", "Joao"]
 lista_usuarios.sort()
 
 app.config["SECRET_KEY"] = "d718df7d5300f2ec0a950cd9b804445b"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}"
+
+database = SQLAlchemy(app)
 
 @app.route("/")
 def home():
@@ -24,10 +42,28 @@ def usuarios():
     return render_template("usuarios.html", lista_usuarios=lista_usuarios)
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form_login = FormLogin()
     form_criarconta = FormCriarConta()
+
+    if form_login.validate_on_submit() and "botao_submit_login" in request.form:
+        flash(
+            f"Login feito com sucesso no e-mail: {form_login.email.data}",
+            "alert-success",
+        )
+        return redirect(url_for("home"))
+
+    if (
+        form_criarconta.validate_on_submit()
+        and "botao_submit_criarconta" in request.form
+    ):
+        flash(
+            f"Conta criada com sucesso para o e-mail: {form_login.email.data}",
+            "alert-success",
+        )
+        return redirect(url_for("home"))
+
     return render_template(
         "login.html", form_login=form_login, form_criarconta=form_criarconta
     )
